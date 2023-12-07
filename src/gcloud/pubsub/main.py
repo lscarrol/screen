3754f2dcacd87b4a2f46df68d452b8805a932302
oswald <lscarrol@buffalo.edu>
2023-12-07 05:28:28 +0000

    Added pub/sub support

---
+++ src/gcloud/pubsub/main.py
@@ -0,0 +1,31 @@
+import base64
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def index():
+    """Receive and parse Pub/Sub messages."""
+    envelope = request.get_json()
+    if not envelope:
+        msg = "no Pub/Sub message received"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    if not isinstance(envelope, dict) or "message" not in envelope:
+        msg = "invalid Pub/Sub message format"
+        print(f"error: {msg}")
+        return f"Bad Request: {msg}", 400
+
+    pubsub_message = envelope["message"]
+
+    name = "World"
+    if isinstance(pubsub_message, dict) and "data" in pubsub_message:
+        name = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
+
+    print(f"Hello {name}!")
+
+    return ("", 204)
+
