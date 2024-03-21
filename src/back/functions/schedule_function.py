e3ea33cf33dcc2ee1809b15579729b12a33fb0cc
oswald <lscarrol@buffalo.edu>
2024-03-21 03:42:00 +0000

    UI updates and 2FA functions

--- src/back/functions/schedule_function.py
+++ src/back/functions/schedule_function.py
@@ -1,14 +1,16 @@
 import os
 import time
-from google.cloud import vision
+from google.cloud import vision, storage
 import openai
 import firebase_admin
-from firebase_admin import credentials, firestoree
+from firebase_admin import credentials, firestore
 
+# Initialize Firebase
 
-cred = credentials.Certificate('screenr-cd3f7-firebase-adminsdk-bi4cr-9737ee940f.json')
-firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+# Initialize Google Cloud Storage client
+storage_client = storage.Client()
 
 def process_screenshots(api):
     while True:
@@ -31,13 +33,17 @@ def process_screenshots(api):
 
         # Download the screenshot
         download = photo.download()
-        screenshot_path = f'screenshots/{photo.filename}'
-        with open(screenshot_path, 'wb') as opened_file:
-            opened_file.write(download.raw.read())
-            print(f'Downloaded: {photo.filename}')
 
-        # Detect text in the downloaded screenshot
-        detected_text = detect_text(screenshot_path)
+        # Upload the screenshot to Google Cloud Storage
+        bucket_name = 'screenshot_arch'  # Replace with your actual bucket name
+        blob_name = f'screenshots/{photo.filename}'
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_string(download.raw.read(), content_type='image/jpeg')
+        print(f'Uploaded: {photo.filename}')
+
+        # Detect text in the uploaded screenshot
+        detected_text = detect_text_from_uri(f'gs://{bucket_name}/{blob_name}')
         if detected_text:
             print(f'Detected text: {detected_text}')
             call_gpt3(detected_text, None)
@@ -47,11 +53,10 @@ def process_screenshots(api):
 
         time.sleep(60)  # Wait for 60 seconds before processing the next screenshot
 
-def detect_text(image_path):
+def detect_text_from_uri(uri):
     client = vision.ImageAnnotatorClient()
-    with open(image_path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.Image(content=content)
+    image = vision.Image()
+    image.source.image_uri = uri
     response = client.text_detection(image=image)
     texts = response.text_annotations
     if len(texts) > 0:
@@ -60,6 +65,7 @@ def detect_text(image_path):
         return None
 
 def call_gpt3(request, categories):
+    openai.api_key = ""
     response = openai.completions.create(
         model="gpt-3.5-turbo-instruct",
         prompt="What do you think this text is: " + request + " Return the result in this format: Category | Name | Location (if applicable) | Short Description",
